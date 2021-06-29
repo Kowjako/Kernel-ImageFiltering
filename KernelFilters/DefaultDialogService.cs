ac133d2bb04cfd95c969e44e0cd816888bf919dd
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-06-29 14:50:28 +0200

    Add DefaultDialogService class

---
+++ KernelFilters/DefaultDialogService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KernelFilters
+{
+    class DefaultDialogService
+    {
+        public string FilePath { get; set; }
+
+        public bool OpenFileDialog()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Txt files (*.txt)|*.txt";
+            if (ofd.ShowDialog() == true)
+            {
+                FilePath = ofd.FileName;
+                return true;
+            }
+            return false;
+        }
+
+        public bool SaveFileDialog()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == true)
+            {
+                FilePath = sfd.FileName;
+                return true;
+            }
+            return false;
+        }
+
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show($"Message: {message}", "Dialog Service Message", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
