4d026f0636c2f8488d6fd1fc3e0345aa7b6cbfba
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-07-04 18:01:10 +0200

    Fixed Convoluter

--- KernelFilters/Convoluter.cs
+++ KernelFilters/Convoluter.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace KernelFilters
 {
     static class Convoluter
     {
-        public static float Convolute(int[,] image, int[,] kernel, float scale = 1)
+        public static float Convolute(int[,] image, float[,] kernel, float scale = 1)
         {
-            int result = 0;
-            for(int i=0;i<image.Length/3 - 1;i++)
+            float result = 0;
+            for(int i=0;i<image.Length/3;i++)
             {
-                for(int j=0;j<image.Length/3 - 1;j++)
+                for(int j=0;j<image.Length/3;j++)
                 {
-                    result += image[i, j] * kernel[i, j];
+                    result += (image[i, j] * kernel[i, j]);
                 }
             }
-            return scale * result;
+            return result *scale;
         }
     }
 }
