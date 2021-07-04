d37ca0587415a211eb151f97e078fcf7677cc99d
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-07-04 18:01:28 +0200

    Worked Box Blur filter

--- KernelFilters/MatrixFilter/NormalizedBoxBlur.cs
+++ KernelFilters/MatrixFilter/NormalizedBoxBlur.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 
 namespace KernelFilters.MatrixFilter
 {
     class NormalizedBoxBlur : IFilter
     {
-        private int[,] kernel =
+        private float[,] kernel =
         {
-            {-1, -1, -1 },
-            {-1, 8, -1 },
-            {-1, -1, -1}
+            {1, 1, 1},
+            {1, 1, 1},
+            {1, 1, 1}
         };
 
         public ImageSource Filterize(ImageSource sourceImage)
@@ -24,7 +22,17 @@ namespace KernelFilters.MatrixFilter
             startImageBMP = Converter.ImageSourceToBitmap(sourceImage);
             outputImageBMP = new Bitmap(startImageBMP.Width, startImageBMP.Height);
 
-            int[,] kernelPixels = new int[3, 3];
+            int[,] kernelPixelsR = new int[3, 3];
+            int[,] kernelPixelsG = new int[3, 3];
+            int[,] kernelPixelsB = new int[3, 3];
+
+            //float[] a = new float[5];
+            //a[0] = (float)-0.5;
+            //a[1] = (float)-0.5;
+            //a[2] = (float)0;
+            //a[3] = (float)0.5;
+            //a[4] = (float)0.5;
+
 
             for (int x = 1; x < startImageBMP.Width - 1; x++)
             {
@@ -34,16 +42,55 @@ namespace KernelFilters.MatrixFilter
                     {
                         for (int j = -1; j <= 1; j++)
                         {
-                            kernelPixels[i + 1, j + 1] = startImageBMP.GetPixel(x + j, y + i).ToArgb();
+                            kernelPixelsR[i + 1, j + 1] = startImageBMP.GetPixel(x + j, y + i).R;
+                            kernelPixelsG[i + 1, j + 1] = startImageBMP.GetPixel(x + j, y + i).G;
+                            kernelPixelsB[i + 1, j + 1] = startImageBMP.GetPixel(x + j, y + i).B;
                         }
                     }
 
-                    int resultPixel = (int)Convoluter.Convolute(kernelPixels, kernel);
-                    if (resultPixel > 255) resultPixel = 255;
-                    if (resultPixel < 0) resultPixel = 0;
+                    //for (int i = -1; i <= 1; i++)
+                    //{
+                    //    for (int j = -1; j <= 1; j++)
+                    //    {
+                    //        MessageBox.Show(kernelPixels[i + 1, j + 1].ToString());
+                    //    }
+                    //}
 
-                    System.Drawing.Color resultColor = System.Drawing.Color.FromArgb(resultPixel);
+                    //MessageBox.Show(Convoluter.Convolute(kernelPixels, kernel).ToString());
+                    int resultPixelR = (int)Convoluter.Convolute(kernelPixelsR, kernel, 1.0f/9);
+                    int resultPixelG = (int)Convoluter.Convolute(kernelPixelsG, kernel, 1.0f / 9);
+                    int resultPixelB = (int)Convoluter.Convolute(kernelPixelsB, kernel, 1.0f / 9);
+
+                    //MessageBox.Show(resultPixel.ToString());
+
+                    if (resultPixelR < 0) resultPixelR = 0;
+                    else if (resultPixelR > 255) resultPixelR = 255;
+                    if (resultPixelG < 0) resultPixelG = 0;
+                    else if (resultPixelG > 255) resultPixelG = 255;
+                    if (resultPixelB < 0) resultPixelB = 0;
+                    else if (resultPixelB > 255) resultPixelB = 255;
+
+                    System.Drawing.Color resultColor = System.Drawing.Color.FromArgb(resultPixelR,resultPixelG, resultPixelB);
                     outputImageBMP.SetPixel(x, y, resultColor);
+
+                    //System.Drawing.Color w1 = startImageBMP.GetPixel(x - 1, y);
+                    //System.Drawing.Color w2 = startImageBMP.GetPixel(x + 1, y);
+                    //System.Drawing.Color w3 = startImageBMP.GetPixel(x, y - 1);
+                    //System.Drawing.Color w4 = startImageBMP.GetPixel(x, y + 1);
+                    //System.Drawing.Color w = startImageBMP.GetPixel(x, y);
+
+                    //int x1 = w1.R;
+                    //int x2 = w2.R;
+                    //int x3 = w3.R;
+                    //int x4 = w4.R;
+                    //int xg = w.R;
+
+                    //int xb = (int)(a[0] * xg);
+                    //xb = (int)(xb + a[1] * x1 + a[2] * x2 + a[3] * x3 + a[4] * x4);
+                    //if (xb < 0) xb = 0;
+                    //else if (xb > 255) xb = 255;
+                    //System.Drawing.Color wb = System.Drawing.Color.FromArgb(xb, xb, xb);
+                    //outputImageBMP.SetPixel(x, y, wb);
                 }
             }
             return Converter.BitmapToImageSource(outputImageBMP);
