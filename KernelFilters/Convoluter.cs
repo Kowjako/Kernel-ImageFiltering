159db3971e396d41915f0a49669b65bf38abc357
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-07-04 14:22:16 +0200

    Add convoluter class for using with image & kernel

---
+++ KernelFilters/Convoluter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KernelFilters
+{
+    static class Convoluter
+    {
+        static float Convolute(int[,] image, int[,] kernel, float scale = 1)
+        {
+            int result = 0;
+            for(int i=0;i<image.Length;i++)
+            {
+                for(int j=0;j<image.Length;j++)
+                {
+                    result += image[i, j] * kernel[i, j];
+                }
+            }
+            return result * scale;
+        }
+    }
+}
