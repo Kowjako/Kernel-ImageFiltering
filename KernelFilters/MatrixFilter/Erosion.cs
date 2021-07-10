31e6eeb52f6a04fd12600b9d4c2a8476ce2b1e77
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-07-10 12:51:37 +0200

    Add erosion filter

---
+++ KernelFilters/MatrixFilter/Erosion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace KernelFilters.MatrixFilter
+{
+    class Erosion : IFilter, IKernelFilter
+    {
+        private float[,] kernel =
+        {
+            {0, 0, 1, 0, 0},
+            {0, 1, 1, 1, 0},
+            {1, 1, 1, 1, 1},
+            {0, 1, 1, 1, 0},
+            {0, 0, 1, 0, 0}
+        };
+
+        float[,] IKernelFilter.kernel
+        {
+            get
+            {
+                return kernel;
+            }
+        }
+
+        public ImageSource Filterize(ImageSource sourceImage)
+        {
+            MatrixConvoluator mc = new MatrixConvoluator(this, sourceImage, 5, 1);
+            return mc.Convoluate();
+        }
+    }
+}
