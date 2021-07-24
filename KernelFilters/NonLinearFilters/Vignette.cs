be4c7dfed119fef225d241660d7a560b3446971c
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-07-24 12:27:52 +0200

    Worked vignette algorithm

--- KernelFilters/NonLinearFilters/Vignette.cs
+++ KernelFilters/NonLinearFilters/Vignette.cs
@@ -16,39 +16,37 @@ namespace KernelFilters.NonLinearFilters
         {
             draw.Bitmap startImageBMP = Converter.ImageSourceToBitmap(sourceImage);
             draw.Bitmap outputImageBMP = new draw.Bitmap(startImageBMP.Width, startImageBMP.Height);
+            double radius = 1;
+            double cx = (double)startImageBMP.Width / 2, cy = (double)startImageBMP.Height / 2;
+            double maxDistance = radius * EuclideanDistance(new Point(0, 0), new Point(cx, cy));
+            double temp;
 
+            for (int j = 0; j < startImageBMP.Height; j++)
+            {
+                for (int i = 0; i < startImageBMP.Width; i++)
+                {
+                    temp = MyCosinus(EuclideanDistance(new Point(cx, cy), new Point(i, j)) / maxDistance);
+                    temp = Math.Pow(temp, 3.5);
+                    outputImageBMP.SetPixel(i, j, draw.Color.FromArgb(255, (byte)(startImageBMP.GetPixel(i, j).R * temp), (byte)(startImageBMP.GetPixel(i, j).G * temp), (byte)(startImageBMP.GetPixel(i, j).B * temp)));
+                }
+            }
+            return Converter.BitmapToImageSource(outputImageBMP);
+        }
 
-            return Converter.BitmapToImageSource(startImageBMP);
+        private double MyCosinus(double x)
+        {
+            x += 1.5707;
+            if (x > Math.PI)
+                x -= 2 * Math.PI;
+            if(x < 0)
+                return 1.27323954 * x + 0.405284735 * x * x;
+            else
+                return 1.27323954 * x - 0.405284735 * x * x;
         }
 
         private double EuclideanDistance(Point a, Point b)
         {
             return Math.Sqrt(Math.Pow((double)(a.X - b.X), 2) + Math.Pow((double)(a.Y - b.Y), 2));
-        }
-
-        private double GetMaxDistanceFromConrners(draw.Bitmap image, Point center)
-        {
-            /* Tworzenie pointów na rogach obrazku */
-            Point[] points = new Point[4];
-            points[0] = new Point(0, 0);
-            points[1] = new Point(image.Width, 0);
-            points[2] = new Point(0, image.Height);
-            points[3] = new Point(image.Width, image.Height);
-
-            /* Obliczenie najbardziej oddalonego pointu od centrum */
-            double maxDistance = 0;
-            for (int i = 0; i < 4; i++)
-            {
-                double distance = EuclideanDistance(points[i], center);
-                if (distance > maxDistance)
-                    maxDistance = distance;
-            }
-            return maxDistance;
-        }
-
-        void GenerateGradient(int[][] matrix)
-        {
-            Point fisrtPoint = new Point(matrix.GetLength(1) / 2, matrix.GetLength(0) / 2);   /* GetLength(1) - ilość kolumn, GetLength(0) - ilość wierszy */
         }
     }
 }
